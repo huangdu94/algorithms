6889bd497718fc3f50d0fe037abb0f4db57fa9ae
依韻 <huangdu.hd@alibaba-inc.com>
2021-03-26 17:31:42 +0800

    昨日一题。

--- src/main/java/work/huangdu/question_bank/easy/DeleteDuplicates.java
+++ src/main/java/work/huangdu/question_bank/easy/DeleteDuplicates.java
@@ -43,4 +43,16 @@ public class DeleteDuplicates {
         }
         return head;
     }
+
+    public ListNode deleteDuplicates3(ListNode head) {
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur.next != null && cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
 }
