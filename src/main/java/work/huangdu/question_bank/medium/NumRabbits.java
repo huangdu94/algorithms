40e1734c41c51f3b2b0ad4df3af48fca2311006f
依韻 <huangdu.hd@alibaba-inc.com>
2021-04-04 10:44:22 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/medium/NumRabbits.java
@@ -0,0 +1,54 @@
+package work.huangdu.question_bank.medium;
+
+import java.util.Arrays;
+
+/**
+ * 781. 森林中的兔子
+ * 森林中，每个兔子都有颜色。其中一些兔子（可能是全部）告诉你还有多少其他的兔子和自己有相同的颜色。我们将这些回答放在 answers 数组里。
+ * 返回森林中兔子的最少数量。
+ * 示例:
+ * 输入: answers = [1, 1, 2]
+ * 输出: 5
+ * 解释:
+ * 两只回答了 "1" 的兔子可能有相同的颜色，设为红色。
+ * 之后回答了 "2" 的兔子不会是红色，否则他们的回答会相互矛盾。
+ * 设回答了 "2" 的兔子为蓝色。
+ * 此外，森林中还应有另外 2 只蓝色兔子的回答没有包含在数组中。
+ * 因此森林中兔子的最少数量是 5: 3 只回答的和 2 只没有回答的。
+ * 输入: answers = [10, 10, 10]
+ * 输出: 11
+ * 输入: answers = []
+ * 输出: 0
+ * 说明:
+ * answers 的长度最大为1000。
+ * answers[i] 是在 [0, 999] 范围内的整数。
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2021/4/4
+ */
+public class NumRabbits {
+    public int numRabbits(int[] answers) {
+        Arrays.sort(answers);
+        int n = answers.length, sum = 0, pre = -1, count = 0;
+        // 跳过开头为0的，正好可以共用一个变量
+        while (answers[sum] == 0) {
+            sum++;
+        }
+        for (int i = sum; i < n; i++) {
+            if (answers[i] != pre || count == 0) {
+                count = answers[i];
+                pre = count;
+                sum += (count + 1);
+            } else {
+                count--;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        NumRabbits nr = new NumRabbits();
+        int[] answers = {10, 10, 10};
+        System.out.println(nr.numRabbits(answers));
+    }
+}
--- src/main/java/work/huangdu/temp/Solution.java
+++ src/main/java/work/huangdu/temp/Solution.java
@@ -1,14 +1,91 @@
 package work.huangdu.temp;
 
+import java.util.Arrays;
+
 /**
  * @author yiyun (huangdu.hd@alibaba-inc.com)
  * @date 2021/3/12
  */
 public class Solution {
+    private int n;
+    private int max;
+    private int[][] ans;
+    private boolean[] used;
+    private int mid;
+    // 一行的值
+    private int standard;
 
+    public int[][] sudoku(int n) {
+        this.n = n;
+        this.max = n * n;
+        this.ans = new int[n][n];
+        this.used = new boolean[max + 1];
+        // 中间数必固定
+        this.mid = (max + 1) / 2;
+        ans[n / 2][n / 2] = mid;
+        used[mid] = true;
+        this.standard = n * (max + 1) / 2;
+        backtrack(0);
+        return ans;
+    }
+
+    public boolean backtrack(int idx) {
+        // 跳过中间一个数，它是确定的
+        if (idx == mid - 1) { idx++;}
+        if (idx == max) { return true; }
+        int row = idx / n, coj = idx % n;
+        for (int num = 1; num <= max; num++) {
+            if (!used[num] && check(row, coj, num)) {
+                ans[row][coj] = num;
+                used[num] = true;
+                if (backtrack(idx + 1)) {
+                    return true;
+                }
+                used[num] = false;
+            }
+        }
+        return false;
+    }
+
+    public boolean check(int row, int coj, int num) {
+        int sum;
+        // 检查行
+        if (coj == n - 1) {
+            sum = num;
+            for (int j = 0; j < n - 1; j++) {
+                sum += ans[row][j];
+            }
+            if (sum != standard) {return false;}
+        }
+        // 检查列
+        if (row == n - 1) {
+            sum = num;
+            for (int i = 0; i < n - 1; i++) {
+                sum += ans[i][coj];
+            }
+            if (sum != standard) {return false;}
+        }
+        // 检查对角线 或 反对角线
+        if (row == n - 1) {
+            if (coj == n - 1) {
+                sum = num;
+                for (int i = 0; i < n - 1; i++) { sum += ans[i][i]; }
+                return sum == standard;
+            } else if (coj == 0) {
+                sum = num;
+                for (int i = 0; i < n - 1; i++) { sum += ans[i][n - i - 1]; }
+                return sum == standard;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
+        long start = System.currentTimeMillis();
+        System.out.println(Arrays.deepToString(solution.sudoku(5)));
+        long end = System.currentTimeMillis();
+        System.out.println("耗时：" + (end - start) / 1000.0 + "秒.");
         System.out.println("Done!");
     }
 }
