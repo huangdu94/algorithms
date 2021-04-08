3975c38efc485b24ac2cd54d961b3cf55ef8e2ad
依韻 <huangdu.hd@alibaba-inc.com>
2021-04-08 10:02:41 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/medium/FindMin.java
@@ -0,0 +1,65 @@
+package work.huangdu.question_bank.medium;
+
+/**
+ * 153. 寻找旋转排序数组中的最小值
+ * 已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,2,4,5,6,7] 在变化后可能得到：
+ * 若旋转 4 次，则可以得到 [4,5,6,7,0,1,2]
+ * 若旋转 4 次，则可以得到 [0,1,2,4,5,6,7]
+ * 注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+ * 给你一个元素值 互不相同 的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
+ * 示例 1：
+ * 输入：nums = [3,4,5,1,2]
+ * 输出：1
+ * 解释：原数组为 [1,2,3,4,5] ，旋转 3 次得到输入数组。
+ * 示例 2：
+ * 输入：nums = [4,5,6,7,0,1,2]
+ * 输出：0
+ * 解释：原数组为 [0,1,2,4,5,6,7] ，旋转 4 次得到输入数组。
+ * 示例 3：
+ * 输入：nums = [11,13,15,17]
+ * 输出：11
+ * 解释：原数组为 [11,13,15,17] ，旋转 4 次得到输入数组。
+ * 提示：
+ * n == nums.length
+ * 1 <= n <= 5000
+ * -5000 <= nums[i] <= 5000
+ * nums 中的所有整数 互不相同
+ * nums 原来是一个升序排序的数组，并进行了 1 至 n 次旋转
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2021/4/8
+ */
+public class FindMin {
+    public int findMin(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            if (nums[i] < nums[j]) {break;}
+            int mid = i + (j - i) / 2;
+            if (nums[i] <= nums[mid]) {
+                i = mid + 1;
+            } else {
+                j = mid;
+            }
+        }
+        return nums[i];
+    }
+
+    public int findMin2(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (nums[pivot] < nums[high]) {
+                high = pivot;
+            } else {
+                low = pivot + 1;
+            }
+        }
+        return nums[low];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        System.out.println(new FindMin().findMin2(nums));
+    }
+}
