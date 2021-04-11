902a3a020e5c81e0bfadb26418ebf5c99685b9bd
依韻 <huangdu.hd@alibaba-inc.com>
2021-04-11 14:59:31 +0800

    每日一题+ 干掉一个TODO

--- src/main/java/work/huangdu/question_bank/medium/Search.java
+++ src/main/java/work/huangdu/question_bank/medium/Search.java
@@ -3,7 +3,8 @@ package work.huangdu.question_bank.medium;
 /**
  * 81. 搜索旋转排序数组 II
  * 已知存在一个按非降序排列的整数数组 nums ，数组中的值不必互不相同。
- * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转 ，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,4,4,5,6,6,7] 在下标 5 处经旋转后可能变为 [4,5,6,6,7,0,1,2,4,4] 。
+ * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转 ，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,4,4,5,6,6,7] 在下标 5 处经旋转后可能变为
+ * [4,5,6,6,7,0,1,2,4,4] 。
  * 给你 旋转后 的数组 nums 和一个整数 target ，请你编写一个函数来判断给定的目标值是否存在于数组中。如果 nums 中存在这个目标值 target ，则返回 true ，否则返回 false 。
  * 示例 1：
  * 输入：nums = [2,5,6,0,0,1,2], target = 0
@@ -24,13 +25,48 @@ package work.huangdu.question_bank.medium;
  * @date 2021/4/7
  */
 public class Search {
-    // TODO 官方题解二分法
+    // 二分
     public boolean search(int[] nums, int target) {
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == target) { return true; }
+            if (nums[i] == nums[mid] && nums[mid] == nums[j]) {
+                i++;
+                j--;
+                continue;
+            }
+            if (nums[mid] <= nums[j]) {
+                if (nums[mid] < target && target <= nums[j]) {
+                    i = mid + 1;
+                } else {
+                    j = mid - 1;
+                }
+            } else {
+                if (nums[i] <= target && target < nums[mid]) {
+                    j = mid - 1;
+                } else {
+                    i = mid + 1;
+                }
+            }
+        }
+        return nums[i] == target;
+    }
+
+    // 暴力
+    public boolean search2(int[] nums, int target) {
         for (int num : nums) {
             if (num == target) {
                 return true;
             }
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+        Search search = new Search();
+        int[] nums = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1};
+        int target = 2;
+        System.out.println(search.search(nums, target));
     }
 }
