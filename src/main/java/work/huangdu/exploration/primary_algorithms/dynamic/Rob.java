0b5e8b19988485e520fcd842063ee9c51b151472
依韻 <huangdu.hd@alibaba-inc.com>
2021-04-15 18:44:23 +0800

    每日一题

--- src/main/java/work/huangdu/exploration/primary_algorithms/dynamic/Rob.java
+++ src/main/java/work/huangdu/exploration/primary_algorithms/dynamic/Rob.java
@@ -1,6 +1,7 @@
 package work.huangdu.exploration.primary_algorithms.dynamic;
 
 /**
+ * 198. 打家劫舍
  * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
  * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
  * 示例 1：
@@ -22,19 +23,58 @@ package work.huangdu.exploration.primary_algorithms.dynamic;
  */
 public class Rob {
     public int rob(int[] nums) {
-        if (nums == null || nums.length == 0)
-            return 0;
-        if (nums.length == 1)
-            return nums[0];
-        if (nums.length == 2)
-            return Math.max(nums[0], nums[1]);
-        int[] dp = new int[nums.length];
+        int n = nums.length;
+        if (n == 0) { return 0; }
+        if (n == 1) { return nums[0]; }
+        if (n == 2) { return Math.max(nums[0], nums[1]); }
+        int[] dp = new int[n];
         dp[0] = nums[0];
         dp[1] = nums[1];
         dp[2] = nums[0] + nums[2];
-        for (int i = 3; i < nums.length; i++) {
+        for (int i = 3; i < n; i++) {
             dp[i] = Math.max(dp[i - 3], dp[i - 2]) + nums[i];
         }
-        return Math.max(dp[nums.length - 2], dp[nums.length - 1]);
+        return Math.max(dp[n - 2], dp[n - 1]);
+    }
+
+    public int rob2(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {return 0;}
+        if (n == 1) {return nums[0];}
+        if (n == 2) {return Math.max(nums[0], nums[1]);}
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = nums[0] + nums[2];
+        for (int i = 3; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + nums[i], dp[i - 3] + nums[i]));
+        }
+        return dp[n - 1];
+    }
+
+    public int rob3(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {return 0;}
+        if (n == 1) {return nums[0];}
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[n - 1];
+    }
+
+    public int rob4(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {return 0;}
+        if (n == 1) {return nums[0];}
+        int first = nums[0], second = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            int temp = Math.max(second, first + nums[i]);
+            first = second;
+            second = temp;
+        }
+        return second;
     }
 }
