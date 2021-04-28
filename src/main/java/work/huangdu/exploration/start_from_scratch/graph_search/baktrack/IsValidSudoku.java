9f82ddad1f3af71bb24550ed12dbdfbe4b2a71da
依韻 <huangdu.hd@alibaba-inc.com>
2021-04-28 13:27:03 +0800

    每日一题

---
+++ src/main/java/work/huangdu/exploration/start_from_scratch/graph_search/baktrack/IsValidSudoku.java
@@ -0,0 +1,17 @@
+package work.huangdu.exploration.start_from_scratch.graph_search.baktrack;
+
+/**
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2021/4/27
+ */
+public class IsValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        int[] flags = new int[9];
+        //for (int i = 0; i < 9; i++) {
+        //    for (int j = 0; j < 9; j++) {
+        //
+        //    }
+        //}
+        return false;
+    }
+}
