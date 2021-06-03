097964558fa011eb00ea5776373147199f84a184
依韻 <huangdu.hd@alibaba-inc.com>
2021-06-03 10:15:24 +0800

    每日一题.

--- src/main/java/work/huangdu/exploration/start_from_scratch/hashmap/prefixsum/CheckSubarraySum.java
+++ src/main/java/work/huangdu/exploration/start_from_scratch/hashmap/prefixsum/CheckSubarraySum.java
@@ -1,31 +1,77 @@
 package work.huangdu.exploration.start_from_scratch.hashmap.prefixsum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 523. 连续的子数组和
- * 给定一个包含 非负数 的数组和一个目标 整数 k，编写一个函数来判断该数组是否含有连续的子数组，其大小至少为 2，且总和为 k 的倍数，即总和为 n*k，其中 n 也是一个整数。
+ * 给你一个整数数组 nums 和一个整数 k ，编写一个函数来判断该数组是否含有同时满足下述条件的连续子数组：
+ * 子数组大小 至少为 2 ，且
+ * 子数组元素总和为 k 的倍数。
+ * 如果存在，返回 true ；否则，返回 false 。
+ * 如果存在一个整数 n ，令整数 x 符合 x = n * k ，则称 x 是 k 的一个倍数。
  * 示例 1：
- * 输入：[23,2,4,6,7], k = 6
- * 输出：True
- * 解释：[2,4] 是一个大小为 2 的子数组，并且和为 6。
+ * 输入：nums = [23,2,4,6,7], k = 6
+ * 输出：true
+ * 解释：[2,4] 是一个大小为 2 的子数组，并且和为 6 。
  * 示例 2：
- * 输入：[23,2,6,4,7], k = 6
- * 输出：True
- * 解释：[23,2,6,4,7]是大小为 5 的子数组，并且和为 42。
- * 说明：
- * 数组的长度不会超过 10,000 。
- * 你可以认为所有数字总和在 32 位有符号整数范围内。
+ * 输入：nums = [23,2,6,4,7], k = 6
+ * 输出：true
+ * 解释：[23, 2, 6, 4, 7] 是大小为 5 的子数组，并且和为 42 。
+ * 42 是 6 的倍数，因为 42 = 7 * 6 且 7 是一个整数。
+ * 示例 3：
+ * 输入：nums = [23,2,6,4,7], k = 13
+ * 输出：false
+ * 提示：
+ * 1 <= nums.length <= 10^5
+ * 0 <= nums[i] <= 10^9
+ * 0 <= sum(nums[i]) <= 2^31 - 1
+ * 1 <= k <= 2^31 - 1
  *
  * @author yiyun (huangdu.hd@alibaba-inc.com)
- * @date 2020/11/7 12:54
+ * @date 2021/6/2
  */
 public class CheckSubarraySum {
     public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length, remainder = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for (int i = 0; i < n; i++) {
+            remainder = (remainder + nums[i]) % k;
+            if (map.containsKey(remainder)) {
+                if (i - map.get(remainder) >= 2) {
+                    return true;
+                }
+            } else {
+                map.put(remainder, i);
+            }
+        }
+        return false;
+    }
+
+    public boolean checkSubarraySum2(int[] nums, int k) {
         int n = nums.length;
         for (int i = 0; i < n; i++) {
             int sum = nums[i];
             for (int j = i + 1; j < n; j++) {
                 sum += nums[j];
-                if (sum == k || k != 0 && sum % k == 0) {
+                if (sum % k == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkSubarraySum3(int[] nums, int k) {
+        int n = nums.length;
+        int[] prefixSum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if ((prefixSum[j + 1] - prefixSum[i]) % k == 0) {
                     return true;
                 }
             }
