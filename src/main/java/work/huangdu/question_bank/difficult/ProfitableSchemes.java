9afe77e01ea2d9f11ec70fbabd86c7bfb38b24a0
依韻 <huangdu.hd@alibaba-inc.com>
2021-06-09 15:59:07 +0800

    每日一题.

--- src/main/java/work/huangdu/question_bank/difficult/ProfitableSchemes.java
+++ src/main/java/work/huangdu/question_bank/difficult/ProfitableSchemes.java
@@ -75,4 +75,44 @@ public class ProfitableSchemes {
         System.out.println(ps.profitableSchemes(n, minProfit, group, profit));
         // 692206787
     }
+
+    public int profitableSchemes2(int n, int minProfit, int[] group, int[] profit) {
+        int len = group.length, MOD = (int)1e9 + 7;
+        int[][][] dp = new int[len + 1][n + 1][minProfit + 1];
+        dp[0][0][0] = 1;
+        for (int i = 1; i <= len; i++) {
+            int members = group[i - 1], earn = profit[i - 1];
+            for (int j = 0; j <= n; j++) {
+                for (int k = 0; k <= minProfit; k++) {
+                    if (j < members) {
+                        dp[i][j][k] = dp[i - 1][j][k];
+                    } else {
+                        dp[i][j][k] = (dp[i - 1][j][k] + dp[i - 1][j - members][Math.max(0, k - earn)]) % MOD;
+                    }
+                }
+            }
+        }
+        int sum = 0;
+        for (int j = 0; j <= n; j++) {
+            sum = (sum + dp[len][j][minProfit]) % MOD;
+        }
+        return sum;
+    }
+
+    public int profitableSchemes3(int n, int minProfit, int[] group, int[] profit) {
+        int[][] dp = new int[n + 1][minProfit + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        int len = group.length, MOD = (int)1e9 + 7;
+        for (int i = 1; i <= len; i++) {
+            int members = group[i - 1], earn = profit[i - 1];
+            for (int j = n; j >= members; j--) {
+                for (int k = minProfit; k >= 0; k--) {
+                    dp[j][k] = (dp[j][k] + dp[j - members][Math.max(0, k - earn)]) % MOD;
+                }
+            }
+        }
+        return dp[n][minProfit];
+    }
 }
