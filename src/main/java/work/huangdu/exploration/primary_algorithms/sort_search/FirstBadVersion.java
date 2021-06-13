0ecb9a00b39e333a8e1fbeecf8f3c1511aa58c32
依韻 <huangdu.hd@alibaba-inc.com>
2021-06-13 13:33:09 +0800

    每日一题。

--- src/main/java/work/huangdu/exploration/primary_algorithms/sort_search/FirstBadVersion.java
+++ src/main/java/work/huangdu/exploration/primary_algorithms/sort_search/FirstBadVersion.java
@@ -1,21 +1,18 @@
 package work.huangdu.exploration.primary_algorithms.sort_search;
 
 /**
+ * 278. 第一个错误的版本
  * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
- * <p>
  * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
- * <p>
  * 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
- * <p>
  * 示例:
- * <p>
  * 给定 n = 5，并且 version = 4 是第一个错误的版本。
- * <p>
  * 调用 isBadVersion(3) -> false
  * 调用 isBadVersion(5) -> true
  * 调用 isBadVersion(4) -> true
- * <p>
  * 所以，4 是第一个错误的版本。
+ * The isBadVersion API is defined in the parent class VersionControl.
+ * boolean isBadVersion(int version);
  *
  * @author yiyun (huangdu.hd@alibaba-inc.com)
  * @date 2020/6/23 11:22
@@ -25,23 +22,31 @@ public class FirstBadVersion extends VersionControl {
         super(count, firstVersion);
     }
 
+    public int firstBadVersion(int n) {
+        int i = 1, j = n;
+        while (i < j) {
+            int version = i + ((j - i) >> 1);
+            if (isBadVersion(version)) {
+                j = version;
+            } else {
+                i = version + 1;
+            }
+        }
+        return i;
+    }
+
     public int firstBadVersion1(int n) {
         int cur = 0;
-        while (++cur < n)
-            if (isBadVersion(cur))
-                return cur;
+        while (++cur < n) { if (isBadVersion(cur)) { return cur; } }
         return n;
     }
 
-    public int firstBadVersion(int n) {
+    public int firstBadVersion2(int n) {
         int left = 1;
         int right = n;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (isBadVersion(mid))
-                right = mid;
-            else
-                left = mid + 1;
+            if (isBadVersion(mid)) { right = mid; } else { left = mid + 1; }
         }
         return left;
     }
@@ -66,8 +71,7 @@ class VersionControl {
      * 检测是否是有问题的版本
      */
     protected boolean isBadVersion(int version) {
-        if (version > count || version < 0)
-            throw new RuntimeException();
+        if (version > count || version < 0) { throw new RuntimeException(); }
         return version >= firstVersion;
     }
 }
