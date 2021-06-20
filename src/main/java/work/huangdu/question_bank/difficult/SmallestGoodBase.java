440122896adbcf39c0b00e57cd5e8931bc998e5b
依韻 <huangdu.hd@alibaba-inc.com>
2021-06-20 21:13:50 +0800

    四天的每日一题. 二分法。

--- src/main/java/work/huangdu/question_bank/difficult/SmallestGoodBase.java
+++ src/main/java/work/huangdu/question_bank/difficult/SmallestGoodBase.java
@@ -41,4 +41,52 @@ public class SmallestGoodBase {
         }
         return Long.toString(nVal - 1);
     }
+
+    public String smallestGoodBase2(String n) {
+        //将字符串转为long类型
+        long num = Long.parseLong(n);
+        //求得数位1的最大数量，即转为2进制时对应的数量
+        int mMax = (int)Math.ceil(Math.log(num) / Math.log(2));
+        for (int m = mMax; m > 1; m--) {
+            //二分法查找是否存在对应k进制，第一个找到存在的就是答案
+            long left = 2;
+            long right = num - 1;
+            while (left <= right) {
+                long mid = left + ((right - left) >> 1);
+                if (goodBaseVal(m, mid) == num) {
+                    return Long.toString(mid);
+                }
+                //溢出时也说明要找的k进制比当前mid小
+                long val = goodBaseVal(m, mid);
+                if (val == num) {
+                    return Long.toString(left);
+                }
+                if (val == -1 || val > num) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return Long.toString(num - 1);
+    }
+
+    //求 数位1数量为 n , 进制为 k 时对应的值
+    public long goodBaseVal(int n, long k) {
+        long sum = 1;
+        long cur = 1;
+        for (int i = 1; i < n; i++) {
+            //溢出时返回-1
+            if (cur > Long.MAX_VALUE / k) { return -1; }
+            cur *= k;
+            if (sum > Long.MAX_VALUE - cur) { return -1; }
+            sum += cur;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        SmallestGoodBase sgb = new SmallestGoodBase();
+        System.out.println(sgb.smallestGoodBase2("15"));
+    }
 }
