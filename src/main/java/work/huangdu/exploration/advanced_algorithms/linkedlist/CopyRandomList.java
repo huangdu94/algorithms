4d3e46ea0a240c8c360ff9aef2b16c6b2f8e2587
依韻 <huangdu.hd@alibaba-inc.com>
2021-07-22 15:25:00 +0800

    每日一题

--- src/main/java/work/huangdu/exploration/advanced_algorithms/linkedlist/CopyRandomList.java
+++ src/main/java/work/huangdu/exploration/advanced_algorithms/linkedlist/CopyRandomList.java
@@ -6,12 +6,15 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 复制带随机指针的链表
- * 给定一个链表，每个节点包含一个额外增加的随机指针，该指针可以指向链表中的任何节点或空节点。
- * 要求返回这个链表的深拷贝。
- * 我们用一个由n个节点组成的链表来表示输入/输出中的链表。每个节点用一个[val, random_index]表示：
- * val：一个表示Node.val的整数。
- * random_index：随机指针指向的节点索引（范围从0到n-1）；如果不指向任何节点，则为null。
+ * 138. 复制带随机指针的链表
+ * 给你一个长度为 n 的链表，每个节点包含一个额外增加的随机指针 random ，该指针可以指向链表中的任何节点或空节点。
+ * 构造这个链表的 深拷贝。 深拷贝应该正好由 n 个 全新 节点组成，其中每个新节点的值都设为其对应的原节点的值。新节点的 next 指针和 random 指针也都应指向复制链表中的新节点，并使原链表和复制链表中的这些指针能够表示相同的链表状态。复制链表中的指针都不应指向原链表中的节点 。
+ * 例如，如果原链表中有 X 和 Y 两个节点，其中 X.random --> Y 。那么在复制链表中对应的两个节点 x 和 y ，同样有 x.random --> y 。
+ * 返回复制链表的头节点。
+ * 用一个由 n 个节点组成的链表来表示输入/输出中的链表。每个节点用一个 [val, random_index] 表示：
+ * val：一个表示 Node.val 的整数。
+ * random_index：随机指针指向的节点索引（范围从 0 到 n-1）；如果不指向任何节点，则为  null 。
+ * 你的代码 只 接受原链表的头节点 head 作为传入参数。
  * 示例 1：
  * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
  * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
@@ -26,17 +29,16 @@ import java.util.Map;
  * 输出：[]
  * 解释：给定的链表为空（空指针），因此返回 null。
  * 提示：
+ * 0 <= n <= 1000
  * -10000 <= Node.val <= 10000
- * Node.random为空（null）或指向链表中的节点。
- * 节点数目不超过 1000 。
- * Java
+ * Node.random 为空（null）或指向链表中的节点。
  *
  * @author yiyun (huangdu.hd@alibaba-inc.com)
  * @date 2020/8/4 9:58
  */
 public class CopyRandomList {
     public Node copyRandomList(Node head) {
-        if (head == null) return null;
+        if (head == null) { return null; }
         // 1. 编织list，将每一个拷贝节点放到原节点的next位置，拼成一个list
         Node originNode = head;
         while (originNode != null) {
@@ -48,8 +50,7 @@ public class CopyRandomList {
         Node cur = head;
         // 2. 赋值random
         while (cur != null) {
-            if (cur.random != null)
-                cur.next.random = cur.random.next;
+            if (cur.random != null) { cur.next.random = cur.random.next; }
             cur = cur.next.next;
         }
         // 3. 解除编织
@@ -58,16 +59,14 @@ public class CopyRandomList {
         while (cur != null) {
             originNode = cur.next;
             cur.next = cur.next.next;
-            if (cur.next != null)
-                originNode.next = originNode.next.next;
+            if (cur.next != null) { originNode.next = originNode.next.next; }
             cur = cur.next;
         }
         return newHead;
     }
 
     public Node copyRandomList2(Node head) {
-        if (head == null)
-            return null;
+        if (head == null) { return null; }
         List<Node> copyNodeList = new ArrayList<>();
         Map<Node, Integer> nodeIndexMap = new HashMap<>();
         Node cur = head;
@@ -82,13 +81,42 @@ public class CopyRandomList {
         cur = head;
         for (int i = 0; i < n; i++) {
             Node curNode = copyNodeList.get(i);
-            if (i != n - 1)
-                curNode.next = copyNodeList.get(i + 1);
-            if (cur.random != null)
-                curNode.random = copyNodeList.get(nodeIndexMap.get(cur.random));
+            if (i != n - 1) { curNode.next = copyNodeList.get(i + 1); }
+            if (cur.random != null) { curNode.random = copyNodeList.get(nodeIndexMap.get(cur.random)); }
             cur = cur.next;
         }
         return copyNodeList.get(0);
+    }
+
+    public Node copyRandomList3(Node head) {
+        if (head == null) {return null;}
+        Node cur = head;
+        while (cur != null) {
+            Node next = cur.next;
+            Node copyNode = new Node(cur.val);
+            copyNode.next = next;
+            cur.next = copyNode;
+            cur = next;
+        }
+        cur = head;
+        while (cur != null) {
+            if (cur.random != null) {
+                cur.next.random = cur.random.next;
+            }
+            cur = cur.next.next;
+        }
+        Node copyHead = head.next;
+        cur = head;
+        while (cur != null) {
+            Node copyNode = cur.next;
+            Node next = copyNode.next;
+            cur.next = next;
+            if (next != null) {
+                copyNode.next = next.next;
+            }
+            cur = next;
+        }
+        return copyHead;
     }
 }
 
