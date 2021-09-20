b26efe70ca77440aa0bba0ea7ba255cdfd5452f3
huangdu.hd <huangdu.hd@alibaba-inc.com>
2021-09-20 19:46:02 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/medium/FindNumberOfLIS.java
@@ -0,0 +1,47 @@
+package work.huangdu.question_bank.medium;
+
+/**
+ * 673. 最长递增子序列的个数
+ * 给定一个未排序的整数数组，找到最长递增子序列的个数。
+ * 示例 1:
+ * 输入: [1,3,5,4,7]
+ * 输出: 2
+ * 解释: 有两个最长递增子序列，分别是 [1, 3, 4, 7] 和[1, 3, 5, 7]。
+ * 示例 2:
+ * 输入: [2,2,2,2,2]
+ * 输出: 5
+ * 解释: 最长递增子序列的长度是1，并且存在5个子序列的长度为1，因此输出5。
+ * 注意: 给定的数组长度不超过 2000 并且结果一定是32位有符号整数。
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2021/9/20
+ */
+public class FindNumberOfLIS {
+    // 复制粘贴需要复习。
+    public int findNumberOfLIS(int[] nums) {
+        int n = nums.length, maxLen = 0, ans = 0;
+        int[] dp = new int[n];
+        int[] cnt = new int[n];
+        for (int i = 0; i < n; ++i) {
+            dp[i] = 1;
+            cnt[i] = 1;
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        cnt[i] = cnt[j]; // 重置计数
+                    } else if (dp[j] + 1 == dp[i]) {
+                        cnt[i] += cnt[j];
+                    }
+                }
+            }
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                ans = cnt[i]; // 重置计数
+            } else if (dp[i] == maxLen) {
+                ans += cnt[i];
+            }
+        }
+        return ans;
+    }
+}
