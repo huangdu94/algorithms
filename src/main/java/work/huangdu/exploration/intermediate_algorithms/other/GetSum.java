f50aae06d75e898fc12ad9fd3608aafcd22f771d
huangdu.hd <huangdu.hd@alibaba-inc.com>
2021-09-27 11:08:15 +0800

    昨日一题。

--- src/main/java/work/huangdu/exploration/intermediate_algorithms/other/GetSum.java
+++ src/main/java/work/huangdu/exploration/intermediate_algorithms/other/GetSum.java
@@ -2,7 +2,7 @@ package work.huangdu.exploration.intermediate_algorithms.other;
 
 /**
  * 两整数之和
- * 不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。
+ * 不使用运算符 + 和 - ，计算两整数 a 、b 之和。
  * 示例 1:
  * 输入: a = 1, b = 2
  * 输出: 3
@@ -18,6 +18,32 @@ public class GetSum {
         return a + b;
     }*/
     public int getSum(int a, int b) {
+        int shift = 0, add = 0, result = 0;
+        while (shift < 32 && (a != 0 || b != 0 || add != 0)) {
+            int bita = a & 1, bitb = b & 1;
+            if (bita == 0 && bitb == 0) {
+                if (add != 0) {
+                    result |= (1 << shift);
+                    add = 0;
+                }
+            } else if (bita == 1 && bitb == 1) {
+                if (add != 0) {
+                    result |= (1 << shift);
+                }
+                add = 1;
+            } else {
+                if (add == 0) {
+                    result |= (1 << shift);
+                }
+            }
+            a >>= 1;
+            b >>= 1;
+            shift++;
+        }
+        return result;
+    }
+
+    public int getSum2(int a, int b) {
         int sum = 0;
         int mask = 1;
         // 是否进位
