680931d072783853b795d1dbe891e253f478bc61
huangdu.hd <huangdu.hd@alibaba-inc.com>
2021-12-17 16:12:51 +0800

    复习可怜的小猪。

--- src/main/java/work/huangdu/exploration/start_from_scratch/binary/number_bit_operation/PoorPigs.java
+++ src/main/java/work/huangdu/exploration/start_from_scratch/binary/number_bit_operation/PoorPigs.java
@@ -2,22 +2,49 @@ package work.huangdu.exploration.start_from_scratch.binary.number_bit_operation;
 
 /**
  * 458. 可怜的小猪
- * 有 1000 只水桶，其中有且只有一桶装的含有毒药，其余装的都是水。它们从外观看起来都一样。如果小猪喝了毒药，它会在 15 分钟内死去。
- * 问题来了，如果需要你在一小时内，弄清楚哪只水桶含有毒药，你最少需要多少只猪？
- * 回答这个问题，并为下列的进阶问题编写一个通用算法。
- * 进阶:
- * 假设有 n 只水桶，猪饮水中毒后会在 m 分钟内死亡，你需要多少猪（x）就能在 p 分钟内找出 “有毒” 水桶？这 n 只水桶里有且仅有一只有毒的桶。
- * 提示：
+ * 有 buckets 桶液体，其中 正好 有一桶含有毒药，其余装的都是水。它们从外观看起来都一样。为了弄清楚哪只水桶含有毒药，你可以喂一些猪喝，通过观察猪是否会死进行判断。不幸的是，你只有 minutesToTest 分钟时间来确定哪桶液体是有毒的。
+ * 喂猪的规则如下：
+ * 选择若干活猪进行喂养
  * 可以允许小猪同时饮用任意数量的桶中的水，并且该过程不需要时间。
- * 小猪喝完水后，必须有 m 分钟的冷却时间。在这段时间里，只允许观察，而不允许继续饮水。
- * 任何给定的桶都可以无限次采样（无限数量的猪）。
+ * 小猪喝完水后，必须有 minutesToDie 分钟的冷却时间。在这段时间里，你只能观察，而不允许继续喂猪。
+ * 过了 minutesToDie 分钟后，所有喝到毒药的猪都会死去，其他所有猪都会活下来。
+ * 重复这一过程，直到时间用完。
+ * 给你桶的数目 buckets ，minutesToDie 和 minutesToTest ，返回在规定时间内判断哪个桶有毒所需的 最小 猪数。
+ * 示例 1：
+ * 输入：buckets = 1000, minutesToDie = 15, minutesToTest = 60
+ * 输出：5
+ * 示例 2：
+ * 输入：buckets = 4, minutesToDie = 15, minutesToTest = 15
+ * 输出：2
+ * 示例 3：
+ * 输入：buckets = 4, minutesToDie = 15, minutesToTest = 30
+ * 输出：2
+ * 提示：
+ * 1 <= buckets <= 1000
+ * 1 <= minutesToDie <= minutesToTest <= 100
  *
  * @author yiyun (huangdu.hd@alibaba-inc.com)
  * @date 2020/10/17 11:16
  */
 public class PoorPigs {
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        if (buckets == 1) return 0;
+        if (buckets == 1) {return 0;}
+        int time = minutesToTest / minutesToDie + 1;
+        int max = time, count = 1;
+        while (max < buckets) {
+            max *= time;
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        PoorPigs pp = new PoorPigs();
+        System.out.println(pp.poorPigs(4, 15, 30));
+    }
+
+    public int poorPigs2(int buckets, int minutesToDie, int minutesToTest) {
+        if (buckets == 1) { return 0; }
         int states = minutesToTest / minutesToDie + 1, count = states, res = 1;
         while (count < buckets) {
             count *= states;
