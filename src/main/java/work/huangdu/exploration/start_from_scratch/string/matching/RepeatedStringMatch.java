8a466b2ea004d838830d08e0aea243bdf98970e3
huangdu.hd <huangdu.hd@alibaba-inc.com>
2021-12-22 19:07:57 +0800

    每日一题。

--- src/main/java/work/huangdu/exploration/start_from_scratch/string/matching/RepeatedStringMatch.java
+++ src/main/java/work/huangdu/exploration/start_from_scratch/string/matching/RepeatedStringMatch.java
@@ -45,4 +45,24 @@ public class RepeatedStringMatch {
         }
         return -1;
     }
+
+    public int repeatedStringMatch2(String a, String b) {
+        int aLen = a.length(), bLen = b.length();
+        if (aLen >= bLen && a.contains(b)) {return 1;}
+        int n = 0, index = -aLen, nextIndex;
+        while ((nextIndex = b.indexOf(a, index + aLen)) != -1) {
+            n++;
+            if (n > 1 && nextIndex != index + aLen) { return -1; }
+            index = nextIndex;
+        }
+        b = b.replaceAll(a, "");
+        if (b.length() == 0) {return n;}
+        if (a.startsWith(b) || a.endsWith(b)) {return n + 1;}
+        for (int i = 1, len = b.length(); i < len; i++) {
+            if (a.endsWith(b.substring(0, i)) && a.startsWith(b.substring(i, len))) {
+                return n + 2;
+            }
+        }
+        return -1;
+    }
 }
