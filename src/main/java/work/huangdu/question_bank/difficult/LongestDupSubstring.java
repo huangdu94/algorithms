d5398035eb599e8d6a848048a401be94ae737447
huangdu.hd <huangdu.hd@alibaba-inc.com>
2021-12-23 17:03:25 +0800

    1. 每日一题 2. 复习马拉车算法

---
+++ src/main/java/work/huangdu/question_bank/difficult/LongestDupSubstring.java
@@ -0,0 +1,24 @@
+package work.huangdu.question_bank.difficult;
+
+/**
+ * 1044. 最长重复子串
+ * 给你一个字符串 s ，考虑其所有 重复子串 ：即，s 的连续子串，在 s 中出现 2 次或更多次。这些出现之间可能存在重叠。
+ * 返回 任意一个 可能具有最长长度的重复子串。如果 s 不含重复子串，那么答案为 "" 。
+ * 示例 1：
+ * 输入：s = "banana"
+ * 输出："ana"
+ * 示例 2：
+ * 输入：s = "abcd"
+ * 输出：""
+ * 提示：
+ * 2 <= s.length <= 3 * 10^4
+ * s 由小写英文字母组成
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2021/12/23
+ */
+public class LongestDupSubstring {
+    public String longestDupSubstring(String s) {
+        return "";
+    }
+}
