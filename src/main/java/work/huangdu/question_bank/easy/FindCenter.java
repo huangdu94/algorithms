6faecea3b7e3086a1ce0c341d3416545c14782f8
huangdu.hd <huangdu.hd@alibaba-inc.com>
2022-02-18 16:27:23 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/easy/FindCenter.java
@@ -0,0 +1,31 @@
+package work.huangdu.question_bank.easy;
+
+/**
+ * 1791. 找出星型图的中心节点
+ * 有一个无向的 星型 图，由 n 个编号从 1 到 n 的节点组成。星型图有一个 中心 节点，并且恰有 n - 1 条边将中心节点与其他每个节点连接起来。
+ * 给你一个二维整数数组 edges ，其中 edges[i] = [ui, vi] 表示在节点 ui 和 vi 之间存在一条边。请你找出并返回 edges 所表示星型图的中心节点。
+ * 示例 1：
+ * 输入：edges = [[1,2],[2,3],[4,2]]
+ * 输出：2
+ * 解释：如上图所示，节点 2 与其他每个节点都相连，所以节点 2 是中心节点。
+ * 示例 2：
+ * 输入：edges = [[1,2],[5,1],[1,3],[1,4]]
+ * 输出：1
+ * 提示：
+ * 3 <= n <= 10^5
+ * edges.length == n - 1
+ * edges[i].length == 2
+ * 1 <= ui, vi <= n
+ * ui != vi
+ * 题目数据给出的 edges 表示一个有效的星型图
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2022/2/18
+ */
+public class FindCenter {
+    public int findCenter(int[][] edges) {
+        int u0 = edges[0][0], v0 = edges[0][1], u1 = edges[1][0], v1 = edges[1][1];
+        if (u0 == u1 || u0 == v1) { return u0; }
+        return v0;
+    }
+}
