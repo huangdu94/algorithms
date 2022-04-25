de84b05b06e8e6f1b541225661da8c8306033a28
huangdu.hd <huangdu.hd@alibaba-inc.com>
2022-04-25 11:19:25 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/medium/Solution398.java
@@ -0,0 +1,41 @@
+package work.huangdu.question_bank.medium;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 398. 随机数索引
+ * 给定一个可能含有重复元素的整数数组，要求随机输出给定的数字的索引。 您可以假设给定的数字一定存在于数组中。
+ * 注意：
+ * 数组大小可能非常大。 使用太多额外空间的解决方案将不会通过测试。
+ * 示例:
+ * int[] nums = new int[] {1,2,3,3,3};
+ * Solution solution = new Solution(nums);
+ * // pick(3) 应该返回索引 2,3 或者 4。每个索引的返回概率应该相等。
+ * solution.pick(3);
+ * // pick(1) 应该返回 0。因为只有nums[0]等于1。
+ * solution.pick(1);
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2022/4/25
+ */
+public class Solution398 {
+    // 水塘抽样算法 时间换空间很有意思。
+    private final Map<Integer, List<Integer>> numIndexListMap;
+
+    public Solution398(int[] nums) {
+        this.numIndexListMap = new HashMap<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            List<Integer> indexList = numIndexListMap.computeIfAbsent(nums[i], k -> new ArrayList<>());
+            indexList.add(i);
+        }
+    }
+
+    public int pick(int target) {
+        List<Integer> indexList = numIndexListMap.get(target);
+        return indexList.get((int)(Math.random() * indexList.size()));
+    }
+}
