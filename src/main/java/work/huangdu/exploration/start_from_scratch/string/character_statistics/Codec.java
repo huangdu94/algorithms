00d952350bf41e4cd7ed6a90d78df631824c4e88
huangdu.hd <huangdu.hd@alibaba-inc.com>
2022-06-29 19:47:08 +0800

    每日一题。

--- src/main/java/work/huangdu/exploration/start_from_scratch/string/character_statistics/Codec.java
+++ src/main/java/work/huangdu/exploration/start_from_scratch/string/character_statistics/Codec.java
@@ -16,14 +16,34 @@ import java.util.Random;
  * @date 2020/9/25 11:00
  */
 public class Codec {
+    class Solution {
+        private long id = Long.MIN_VALUE;
+        private Map<String, Long> urlIdMap = new HashMap<>();
+        private Map<Long, String> idUrlMap = new HashMap<>();
+
+        // Encodes a URL to a shortened URL.
+        public String encode(String longUrl) {
+            if (!urlIdMap.containsKey(longUrl)) {
+                idUrlMap.put(id, longUrl);
+                urlIdMap.put(longUrl, id++);
+            }
+            return urlIdMap.get(longUrl).toString();
+        }
+
+        // Decodes a shortened URL to its original URL.
+        public String decode(String shortUrl) {
+            return idUrlMap.get(Long.parseLong(shortUrl));
+        }
+    }
+
     private static final String prefix = "http://tinyurl.com/";
     private static final int ENCODE_LEN = 6;
     private static final char[] base62 = {
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
     private final Random random = new Random();
     private final Map<String, String> map = new HashMap<>();
 
