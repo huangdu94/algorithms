a835c4104a564495e0c2f012ffc62031b733dcab
huangdu94 <tiancaihuangdu@gmail.com>
2022-08-01 22:23:07 +0800

    每日一题

---
+++ src/main/java/work/huangdu/question_bank/easy/GenerateTheString.java
@@ -0,0 +1,40 @@
+package work.huangdu.question_bank.easy;
+
+import java.util.Arrays;
+
+/**
+ * 1374. 生成每种字符都是奇数个的字符串
+ * 给你一个整数 n，请你返回一个含 n 个字符的字符串，其中每种字符在该字符串中都恰好出现 奇数次 。
+ * 返回的字符串必须只含小写英文字母。如果存在多个满足题目要求的字符串，则返回其中任意一个即可。
+ * 示例 1：
+ * 输入：n = 4
+ * 输出："pppz"
+ * 解释："pppz" 是一个满足题目要求的字符串，因为 'p' 出现 3 次，且 'z' 出现 1 次。当然，还有很多其他字符串也满足题目要求，比如："ohhh" 和 "love"。
+ * 示例 2：
+ * 输入：n = 2
+ * 输出："xy"
+ * 解释："xy" 是一个满足题目要求的字符串，因为 'x' 和 'y' 各出现 1 次。当然，还有很多其他字符串也满足题目要求，比如："ag" 和 "ur"。
+ * 示例 3：
+ * 输入：n = 7
+ * 输出："holasss"
+ * 提示：
+ * 1 <= n <= 500
+ *
+ * @author huangdu.hd@alibaba-inc.com
+ * @date 2022/8/1
+ */
+public class GenerateTheString {
+    public String generateTheString(int n) {
+        // 如果n为奇数，直接生成奇数个a即可
+        if ((n & 1) == 1) {
+            char[] chars = new char[n];
+            Arrays.fill(chars, 'a');
+            return new String(chars);
+        }
+        // 如果n为偶数，则生成n-1个a，和1个b即可
+        char[] chars = new char[n];
+        Arrays.fill(chars, 'a');
+        chars[n - 1] = 'b';
+        return new String(chars);
+    }
+}
