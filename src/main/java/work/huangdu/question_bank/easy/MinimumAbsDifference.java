a657041b533a0007e58f0b95de4456ea923ca630
huangdu.hd <huangdu.hd@alibaba-inc.com>
2022-08-11 13:38:44 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/easy/MinimumAbsDifference.java
@@ -0,0 +1,44 @@
+package work.huangdu.question_bank.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 1200. 最小绝对差
+ * 给你个整数数组 arr，其中每个元素都 不相同。
+ * 请你找到所有具有最小绝对差的元素对，并且按升序的顺序返回。
+ * 示例 1：
+ * 输入：arr = [4,2,1,3]
+ * 输出：[[1,2],[2,3],[3,4]]
+ * 示例 2：
+ * 输入：arr = [1,3,6,10,15]
+ * 输出：[[1,3]]
+ * 示例 3：
+ * 输入：arr = [3,8,-10,23,19,-4,-14,27]
+ * 输出：[[-14,-10],[19,23],[23,27]]
+ * 提示：
+ * 2 <= arr.length <= 10^5
+ * -10^6 <= arr[i] <= 10^6
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2022/7/4
+ */
+public class MinimumAbsDifference {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length, min = Integer.MAX_VALUE;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n - 1; i++) {
+            int diff = arr[i + 1] - arr[i];
+            if (diff < min) {
+                min = diff;
+                ans.clear();
+            }
+            if (diff == min) {
+                ans.add(Arrays.asList(arr[i], arr[i + 1]));
+            }
+        }
+        return ans;
+    }
+}
