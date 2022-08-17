61988732a869d53ebb7faef43b4d34937ecf1b28
huangdu.hd <huangdu.hd@alibaba-inc.com>
2022-08-17 11:14:57 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/medium/DeepestLeavesSum.java
@@ -0,0 +1,57 @@
+package work.huangdu.question_bank.medium;
+
+import work.huangdu.data_structure.TreeNode;
+
+/**
+ * 1302. 层数最深叶子节点的和
+ * 给你一棵二叉树的根节点 root ，请你返回 层数最深的叶子节点的和 。
+ * 示例 1：
+ * 输入：root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+ * 输出：15
+ * 示例 2：
+ * 输入：root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+ * 输出：19
+ * 提示：
+ * 树中节点数目在范围 [1, 10^4] 之间。
+ * 1 <= Node.val <= 100
+ * Definition for a binary tree node.
+ * * public class TreeNode {
+ * *     int val;
+ * *     TreeNode left;
+ * *     TreeNode right;
+ * *     TreeNode() {}
+ * *     TreeNode(int val) { this.val = val; }
+ * *     TreeNode(int val, TreeNode left, TreeNode right) {
+ * *         this.val = val;
+ * *         this.left = left;
+ * *         this.right = right;
+ * *     }
+ * * }
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2022/8/17
+ */
+public class DeepestLeavesSum {
+    private int maxDepth = 0;
+    private int sum = 0;
+
+    public int deepestLeavesSum(TreeNode root) {
+        dfs(root, 0);
+        return sum;
+    }
+
+    private void dfs(TreeNode root, int depth) {
+        if (maxDepth == depth) {
+            sum += root.val;
+        } else if (maxDepth < depth) {
+            maxDepth = depth;
+            sum = root.val;
+        }
+        if (root.left != null) {
+            dfs(root.left, depth + 1);
+        }
+        if (root.right != null) {
+            dfs(root.right, depth + 1);
+        }
+    }
+}
