5f80346af8e08281d676411bebe74260b3ac5972
huangdu.hd <huangdu.hd@alibaba-inc.com>
2022-09-10 20:58:38 +0800

    每日一题。

--- src/main/java/work/huangdu/exploration/start_from_scratch/tree/inorder_binary_search_tree/TrimBST.java
+++ src/main/java/work/huangdu/exploration/start_from_scratch/tree/inorder_binary_search_tree/TrimBST.java
@@ -42,6 +42,15 @@ public class TrimBST {
         return root;
     }
 
+    public TreeNode trimBST2(TreeNode root, int low, int high) {
+        if (root == null) {return null;}
+        if (root.val < low) {return trimBST(root.right, low, high);}
+        if (root.val > high) {return trimBST(root.left, low, high);}
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3, new TreeNode(0, null, new TreeNode(2, new TreeNode(1), null)), new TreeNode(4));
         TrimBST tbst = new TrimBST();
