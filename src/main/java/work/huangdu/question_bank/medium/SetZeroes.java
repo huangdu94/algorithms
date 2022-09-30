1b59ca70dc87af5d7a109888b667351f60e72bd3
huangdu.hd <huangdu.hd@alibaba-inc.com>
2022-09-30 13:49:24 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/medium/SetZeroes.java
@@ -0,0 +1,79 @@
+package work.huangdu.question_bank.medium;
+
+import java.util.Arrays;
+
+/**
+ * 面试题 01.08. 零矩阵
+ * 编写一种算法，若M × N矩阵中某个元素为0，则将其所在的行与列清零。
+ * 示例 1：
+ * 输入：
+ * [
+ * [1,1,1],
+ * [1,0,1],
+ * [1,1,1]
+ * ]
+ * 输出：
+ * [
+ * [1,0,1],
+ * [0,0,0],
+ * [1,0,1]
+ * ]
+ * 示例 2：
+ * 输入：
+ * [
+ * [0,1,2,0],
+ * [3,4,5,2],
+ * [1,3,1,5]
+ * ]
+ * 输出：
+ * [
+ * [0,0,0,0],
+ * [0,4,5,0],
+ * [0,3,1,0]
+ * ]
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2022/9/30
+ */
+public class SetZeroes {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean flag = false;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    if (j == 0) {
+                        flag = true;
+                    } else {
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (matrix[0][0] == 0) {
+            for (int i = 0; i < n; i++) {
+                Arrays.fill(matrix[0], 0);
+            }
+        }
+        if (flag) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SetZeroes sz = new SetZeroes();
+        int[][] matrix = {{1, 1, 1}, {0, 1, 2}};
+        sz.setZeroes(matrix);
+        System.out.println();
+    }
+}
