702a8f3698082e14151e8466f949ae1aa0278935
huangdu.hd <huangdu.hd@alibaba-inc.com>
2022-11-25 17:29:09 +0800

    补：每日一题。

--- src/main/java/work/huangdu/question_bank/difficult/LargestComponentSize.java
+++ src/main/java/work/huangdu/question_bank/difficult/LargestComponentSize.java
@@ -1,5 +1,8 @@
 package work.huangdu.question_bank.difficult;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 952. 按公因数计算最大组件大小
  * 给定一个由不同正整数的组成的非空数组 nums ，考虑下面的图：
@@ -24,8 +27,42 @@ package work.huangdu.question_bank.difficult;
  * @date 2022/7/30
  */
 public class LargestComponentSize {
+    private final int[] parent = new int[100001];
+
     public int largestComponentSize(int[] nums) {
-        // TODO
-        return -1;
+        for (int i = 0; i <= 100000; i++) {
+            parent[i] = i;
+        }
+        for (int num : nums) {
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) {
+                    union(num, i);
+                    union(num, num / i);
+                }
+            }
+        }
+        int ans = 0;
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            ans = Math.max(ans, count.merge(find(num), 1, Integer::sum));
+        }
+        return ans;
+    }
+
+    private int find(int x) {
+        if (parent[x] == x) {return x;}
+        return parent[x] = find(parent[x]);
+    }
+
+    private void union(int x, int y) {
+        int parentX = find(x), parentY = find(y);
+        if (parentX != parentY) {
+            parent[parentX] = parentY;
+        }
+    }
+
+    public static void main(String[] args) {
+        LargestComponentSize lcs = new LargestComponentSize();
+        System.out.println(lcs.largestComponentSize(new int[] {4, 6, 15, 35}));
     }
 }
