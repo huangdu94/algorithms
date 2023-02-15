66434622a32c6761932ccb7e81a4cc0014e6a0ae
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-02-15 15:25:59 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/difficult/IsGoodArray.java
@@ -0,0 +1,21 @@
+package work.huangdu.question_bank.difficult;
+
+/**
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2023/2/15
+ */
+public class IsGoodArray {
+    // 裴蜀定理
+    public boolean isGoodArray(int[] nums) {
+        int gcd = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            gcd = gcd(gcd, nums[i]);
+        }
+        return gcd == 1;
+    }
+
+    public int gcd(int a, int b) {
+        if (b == 0) {return a;}
+        return gcd(b, a % b);
+    }
+}
