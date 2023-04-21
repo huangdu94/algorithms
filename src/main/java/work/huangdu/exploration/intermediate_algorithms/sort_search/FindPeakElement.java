9f185ea446a60cfb67c7c9778cf38fd92ff813f8
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-04-21 12:43:43 +0800

    二分训练

--- src/main/java/work/huangdu/exploration/intermediate_algorithms/sort_search/FindPeakElement.java
+++ src/main/java/work/huangdu/exploration/intermediate_algorithms/sort_search/FindPeakElement.java
@@ -1,7 +1,7 @@
 package work.huangdu.exploration.intermediate_algorithms.sort_search;
 
 /**
- * 寻找峰值
+ * 162. 寻找峰值
  * 峰值元素是指其值大于左右相邻值的元素。
  * 给定一个输入数组 nums，其中 nums[i] ≠ nums[i+1]，找到峰值元素并返回其索引。
  * 数组可能包含多个峰值，在这种情况下，返回任何一个峰值所在位置即可。
@@ -23,6 +23,19 @@ package work.huangdu.exploration.intermediate_algorithms.sort_search;
  */
 public class FindPeakElement {
     public int findPeakElement(int[] nums) {
+        int n = nums.length, left = 0, right = nums.length - 2;
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+            if(nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    public int findPeakElement4(int[] nums) {
         int n = nums.length, i = 0, j = n - 1;
         while (i < j) {
             int mid = i + ((j - i) >> 1);
