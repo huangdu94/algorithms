3b9e32a220af79be32bc8ecc3c42efd0f53825d2
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-05-19 11:09:17 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/medium/NumTilePossibilities.java
@@ -0,0 +1,53 @@
+package work.huangdu.question_bank.medium;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 1079. 活字印刷
+ * 你有一套活字字模 tiles，其中每个字模上都刻有一个字母 tiles[i]。返回你可以印出的非空字母序列的数目。
+ * 注意：本题中，每个活字字模只能使用一次。
+ * 示例 1：
+ * 输入："AAB"
+ * 输出：8
+ * 解释：可能的序列为 "A", "B", "AA", "AB", "BA", "AAB", "ABA", "BAA"。
+ * 示例 2：
+ * 输入："AAABBC"
+ * 输出：188
+ * 示例 3：
+ * 输入："V"
+ * 输出：1
+ * 提示：
+ * 1 <= tiles.length <= 7
+ * tiles 由大写英文字母组成
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2023/5/19
+ */
+public class NumTilePossibilities {
+    public int numTilePossibilities(String tiles) {
+        Map<Character, Integer> count = new HashMap<>();
+        for (char t : tiles.toCharArray()) {
+            count.put(t, count.getOrDefault(t, 0) + 1);
+        }
+        Set<Character> tile = new HashSet<>(count.keySet());
+        return dfs(tiles.length(), count, tile) - 1;
+    }
+
+    private int dfs(int i, Map<Character, Integer> count, Set<Character> tile) {
+        if (i == 0) {
+            return 1;
+        }
+        int res = 1;
+        for (char t : tile) {
+            if (count.get(t) > 0) {
+                count.put(t, count.get(t) - 1);
+                res += dfs(i - 1, count, tile);
+                count.put(t, count.get(t) + 1);
+            }
+        }
+        return res;
+    }
+}
