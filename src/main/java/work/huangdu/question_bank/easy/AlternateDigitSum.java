fecce80018eaac6cf8409759b070fed6632a3acc
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-07-12 10:49:51 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/easy/AlternateDigitSum.java
@@ -0,0 +1,42 @@
+package work.huangdu.question_bank.easy;
+
+/**
+ * 2544. 交替数字和
+ * 给你一个正整数 n 。n 中的每一位数字都会按下述规则分配一个符号：
+ * 最高有效位 上的数字分配到 正 号。
+ * 剩余每位上数字的符号都与其相邻数字相反。
+ * 返回所有数字及其对应符号的和。
+ * 示例 1：
+ * 输入：n = 521
+ * 输出：4
+ * 解释：(+5) + (-2) + (+1) = 4
+ * 示例 2：
+ * 输入：n = 111
+ * 输出：1
+ * 解释：(+1) + (-1) + (+1) = 1
+ * 示例 3：
+ * 输入：n = 886996
+ * 输出：0
+ * 解释：(+8) + (-8) + (+6) + (-9) + (+9) + (-6) = 0
+ * 提示：
+ * 1 <= n <= 10^9
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2023/7/12
+ */
+public class AlternateDigitSum {
+    public int alternateDigitSum(int n) {
+        int ans = 0;
+        boolean flag = false;
+        while (n > 0) {
+            if (flag) {
+                ans -= n % 10;
+            } else {
+                ans += n % 10;
+            }
+            flag = !flag;
+            n /= 10;
+        }
+        return flag ? ans : -ans;
+    }
+}
