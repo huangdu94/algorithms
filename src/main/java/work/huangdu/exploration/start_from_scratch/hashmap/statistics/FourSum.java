72d5ca6ef1af7ba304369b68d4f61b1fb56ffabb
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-07-15 18:59:18 +0800

    每日一题。

--- src/main/java/work/huangdu/exploration/start_from_scratch/hashmap/statistics/FourSum.java
+++ src/main/java/work/huangdu/exploration/start_from_scratch/hashmap/statistics/FourSum.java
@@ -1,26 +1,30 @@
 package work.huangdu.exploration.start_from_scratch.hashmap.statistics;
 
-import work.huangdu.exploration.advanced_algorithms.array_string.FourSumCount;
-import work.huangdu.exploration.intermediate_algorithms.array_string.ThreeSum;
-import work.huangdu.exploration.start_from_scratch.hashmap.index.TwoSum2;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import work.huangdu.exploration.advanced_algorithms.array_string.FourSumCount;
+import work.huangdu.exploration.intermediate_algorithms.array_string.ThreeSum;
+import work.huangdu.exploration.start_from_scratch.hashmap.index.TwoSum2;
+
 /**
  * 18. 四数之和
- * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
- * 注意：
- * 答案中不可以包含重复的四元组。
- * 示例：
- * 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
- * 满足要求的四元组集合为：
- * [
- * [-1,  0, 0, 1],
- * [-2, -1, 1, 2],
- * [-2,  0, 0, 2]
- * ]
+ * 给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
+ * 0 <= a, b, c, d < n
+ * a、b、c 和 d 互不相同
+ * nums[a] + nums[b] + nums[c] + nums[d] == target
+ * 你可以按 任意顺序 返回答案 。
+ * 示例 1：
+ * 输入：nums = [1,0,-1,0,-2,2], target = 0
+ * 输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+ * 示例 2：
+ * 输入：nums = [2,2,2,2,2], target = 8
+ * 输出：[[2,2,2,2]]
+ * 提示：
+ * 1 <= nums.length <= 200
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 10^9
  *
  * @author yiyun (huangdu.hd@alibaba-inc.com)
  * @date 2020/10/5 12:07
@@ -32,7 +36,7 @@ import java.util.List;
 public class FourSum {
     // 时间复杂度 o(n^3)
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        if (nums.length < 4) return new ArrayList<>();
+        if (nums.length < 4) {return new ArrayList<>();}
         int n = nums.length;
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
@@ -46,7 +50,7 @@ public class FourSum {
                     continue;
                 }
                 int second = nums[b];
-                int t = target - (first + second);
+                long t = (long)target - (first + second);
                 int i = b + 1, j = n - 1;
                 while (i < j) {
                     if (nums[i] + nums[j] > t) {
