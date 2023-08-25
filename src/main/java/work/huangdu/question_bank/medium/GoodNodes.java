d7f3c78c2458101050befce07c0b6ce66d424d20
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-08-25 16:43:27 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/medium/GoodNodes.java
@@ -0,0 +1,46 @@
+package work.huangdu.question_bank.medium;
+
+import work.huangdu.data_structure.TreeNode;
+
+/**
+ * 1448. 统计二叉树中好节点的数目
+ * 给你一棵根为 root 的二叉树，请你返回二叉树中好节点的数目。
+ * 「好节点」X 定义为：从根到该节点 X 所经过的节点中，没有任何节点的值大于 X 的值。
+ * 示例 1：
+ * 输入：root = [3,1,4,3,null,1,5]
+ * 输出：4
+ * 解释：图中蓝色节点为好节点。
+ * 根节点 (3) 永远是个好节点。
+ * 节点 4 -> (3,4) 是路径中的最大值。
+ * 节点 5 -> (3,4,5) 是路径中的最大值。
+ * 节点 3 -> (3,1,3) 是路径中的最大值。
+ * 示例 2：
+ * 输入：root = [3,3,null,4,2]
+ * 输出：3
+ * 解释：节点 2 -> (3, 3, 2) 不是好节点，因为 "3" 比它大。
+ * 示例 3：
+ * 输入：root = [1]
+ * 输出：1
+ * 解释：根节点是好节点。
+ * 提示：
+ * 二叉树中节点数目范围是 [1, 10^5] 。
+ * 每个节点权值的范围是 [-10^4, 10^4] 。
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ * @date 2023/8/25
+ */
+public class GoodNodes {
+    public int goodNodes(TreeNode root) {
+        return dfs(root, -10000);
+    }
+
+    private int dfs(TreeNode node, int max) {
+        if (node == null) {return 0;}
+        int ans = 0;
+        if (node.val >= max) {
+            max = node.val;
+            ans++;
+        }
+        return dfs(node.left, max) + dfs(node.right, max) + ans;
+    }
+}
