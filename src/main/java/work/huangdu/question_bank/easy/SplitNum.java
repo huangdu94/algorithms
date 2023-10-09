661f8cf6f7756850a9a9f168c89bb2ac41105674
huangdu.hd <huangdu.hd@alibaba-inc.com>
2023-10-09 11:02:34 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/easy/SplitNum.java
@@ -0,0 +1,25 @@
+package work.huangdu.question_bank.easy;
+
+/**
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ */
+public class SplitNum {
+    public int splitNum(int num) {
+        int[] count = new int[10], nums = new int[2];
+        while (num > 0) {
+            count[num % 10]++;
+            num /= 10;
+        }
+        int i = 1, j = 0;
+        while (i < 10) {
+            if (count[i] == 0) {
+                i++;
+                continue;
+            }
+            count[i]--;
+            nums[j & 1] = nums[j & 1] * 10 + i;
+            j++;
+        }
+        return nums[0] + nums[1];
+    }
+}
