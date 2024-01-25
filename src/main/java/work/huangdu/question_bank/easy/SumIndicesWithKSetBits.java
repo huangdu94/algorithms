1268756593908853eb609da1d4c0b3153937a938
依韻 <huangdu.hd@alibaba-inc.com>
2024-01-25 11:00:41 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/easy/SumIndicesWithKSetBits.java
@@ -0,0 +1,49 @@
+package work.huangdu.question_bank.easy;
+
+import java.util.List;
+
+/**
+ * 2859. 计算 K 置位下标对应元素的和
+ * 给你一个下标从 0 开始的整数数组 nums 和一个整数 k 。
+ * 请你用整数形式返回 nums 中的特定元素之和 ，这些特定元素满足：其对应下标的二进制表示中恰存在 k 个置位。
+ * 整数的二进制表示中的 1 就是这个整数的 置位 。
+ * 例如，21 的二进制表示为 10101 ，其中有 3 个置位。
+ * 示例 1：
+ * 输入：nums = [5,10,1,5,2], k = 1
+ * 输出：13
+ * 解释：下标的二进制表示是：
+ * 0 = 000
+ * 1 = 001
+ * 2 = 010
+ * 3 = 011
+ * 4 = 100
+ * 下标 1、2 和 4 在其二进制表示中都存在 k = 1 个置位。
+ * 因此，答案为 nums[1] + nums[2] + nums[4] = 13 。
+ * 示例 2：
+ * 输入：nums = [4,3,2,1], k = 2
+ * 输出：1
+ * 解释：下标的二进制表示是：
+ * 0 = 00
+ * 1 = 01
+ * 2 = 10
+ * 3 = 11
+ * 只有下标 3 的二进制表示中存在 k = 2 个置位。
+ * 因此，答案为 nums[3] = 1 。
+ * 提示：
+ * 1 <= nums.length <= 1000
+ * 1 <= nums[i] <= 10^5
+ * 0 <= k <= 10
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ */
+public class SumIndicesWithKSetBits {
+    public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+        int ans = 0, n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (Integer.bitCount(i) == k) {
+                ans += nums.get(i);
+            }
+        }
+        return ans;
+    }
+}
