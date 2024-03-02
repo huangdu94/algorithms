3e2de768aa95c933a786d8b75e2fbbbf2010f009
依韻 <huangdu.hd@alibaba-inc.com>
2024-03-02 22:19:01 +0800

    每日一题。

---
+++ src/main/java/work/huangdu/question_bank/medium/ReachableNodes.java
@@ -0,0 +1,68 @@
+package work.huangdu.question_bank.medium;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * 2368. 受限条件下可到达节点的数目
+ * 现有一棵由 n 个节点组成的无向树，节点编号从 0 到 n - 1 ，共有 n - 1 条边。
+ * 给你一个二维整数数组 edges ，长度为 n - 1 ，其中 edges[i] = [ai, bi] 表示树中节点 ai 和 bi 之间存在一条边。另给你一个整数数组 restricted 表示 受限 节点。
+ * 在不访问受限节点的前提下，返回你可以从节点 0 到达的 最多 节点数目。
+ * 注意，节点 0 不 会标记为受限节点。
+ * 示例 1：
+ * 输入：n = 7, edges = [[0,1],[1,2],[3,1],[4,0],[0,5],[5,6]], restricted = [4,5]
+ * 输出：4
+ * 解释：上图所示正是这棵树。
+ * 在不访问受限节点的前提下，只有节点 [0,1,2,3] 可以从节点 0 到达。
+ * 示例 2：
+ * 输入：n = 7, edges = [[0,1],[0,2],[0,5],[0,4],[3,2],[6,5]], restricted = [4,2,1]
+ * 输出：3
+ * 解释：上图所示正是这棵树。
+ * 在不访问受限节点的前提下，只有节点 [0,5,6] 可以从节点 0 到达。
+ * 提示：
+ * 2 <= n <= 10^5
+ * edges.length == n - 1
+ * edges[i].length == 2
+ * 0 <= ai, bi < n
+ * ai != bi
+ * edges 表示一棵有效的树
+ * 1 <= restricted.length < n
+ * 1 <= restricted[i] < n
+ * restricted 中的所有值 互不相同
+ *
+ * @author yiyun (huangdu.hd@alibaba-inc.com)
+ */
+public class ReachableNodes {
+    public int reachableNodes(int n, int[][] edges, int[] restricted) {
+        Set<Integer> restrictedSet = new HashSet<>();
+        for (int node : restricted) {restrictedSet.add(node);}
+        List<Integer>[] graph = new List[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int[] edge : edges) {
+            int from = edge[0], to = edge[1];
+            graph[from].add(to);
+            graph[to].add(from);
+        }
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(0);
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            Integer cur = queue.poll();
+            visited[0] = true;
+            ans++;
+            for (Integer next : graph[cur]) {
+                if (!restrictedSet.contains(next) && !visited[next]) {
+                    queue.offer(next);
+                }
+            }
+        }
+        return ans;
+    }
+}
