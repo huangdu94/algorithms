3d18c97f08aa317b3a1e8ffd108d51f26e8eec7e
huangdu <445951954@qq.com>
2025-05-23 07:21:01 +0800

    每日一题， 精简代码。

--- src/main/java/work/huangdu/exploration/primary_algorithms/sort_search/Merge.java
+++ src/main/java/work/huangdu/exploration/primary_algorithms/sort_search/Merge.java
@@ -22,12 +22,12 @@ package work.huangdu.exploration.primary_algorithms.sort_search;
  */
 public class Merge {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m + n - 1, p1 = m - 1, p2 = n - 1;
-        while (p2 >= 0) {
-            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
-                nums1[i--] = nums1[p1--];
+        int i = m - 1, j = n - 1;
+        while (j >= 0) {
+            if (i < 0 || nums2[j] >= nums1[i]) {
+                nums1[i + j + 1] = nums2[j--];
             } else {
-                nums1[i--] = nums2[p2--];
+                nums1[i + j + 1] = nums1[i--];
             }
         }
     }
