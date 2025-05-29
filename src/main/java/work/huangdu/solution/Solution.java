f80cbba7eb5f7a5fbb79c97ed2b920ec04ffb457
huangdu <445951954@qq.com>
2025-05-29 15:30:54 +0800

    增加Solution类，用于临时写算法代码。

--- src/main/java/work/huangdu/solution/Solution.java
+++ src/main/java/work/huangdu/solution/Solution.java
@@ -7,20 +7,7 @@ package work.huangdu.solution;
  * @version 2025/5/27
  */
 public class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-        int n = gas.length, left = 0, right = 0, remain = 0;
-        boolean loop = false;
-        do {
-            remain += gas[right] - cost[right];
-            while (remain < 0) {
-                remain -= gas[left] - cost[left];
-                if ((left = (left + 1) % n) == 0) {
-                    loop = true;
-                    break;
-                }
-            }
-            right = (right + 1) % n;
-        } while (left != right);
-        return loop ? -1 : left;
+
+    public static void main(String[] args) {
     }
 }
