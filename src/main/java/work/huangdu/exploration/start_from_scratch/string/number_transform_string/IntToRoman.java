9689e2181a16fa34fd6bd48e51748ada3d7b9c6e
huangdu <445951954@qq.com>
2025-06-04 12:13:40 +0800

    增加Solution类，用于临时写算法代码。

--- src/main/java/work/huangdu/exploration/start_from_scratch/string/number_transform_string/IntToRoman.java
+++ src/main/java/work/huangdu/exploration/start_from_scratch/string/number_transform_string/IntToRoman.java
@@ -208,4 +208,39 @@ public class IntToRoman {
         }
         return result.toString();
     }
+
+    private static final int N = 7;
+    private static final int[] NUMS = {1000, 500, 100, 50, 10, 5, 1};
+    private static final char[] CHARS = {'M', 'D', 'C', 'L', 'X', 'V', 'I'};
+
+    public String intToRoman3(int num) {
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i < N) {
+            int n = num / NUMS[i];
+            num -= n * NUMS[i];
+            if (n > 0) {
+                if (n < 4) {
+                    char ch = CHARS[i];
+                    for (int k = 0; k < n; k++) {
+                        sb.append(ch);
+                    }
+                } else if (n == 4) {
+                    sb.append(CHARS[i]);
+                    sb.append(CHARS[i - 1]);
+                } else if (n < 9) {
+                    sb.append(CHARS[i - 1]);
+                    char ch = CHARS[i];
+                    for (int k = 5; k < n; k++) {
+                        sb.append(ch);
+                    }
+                } else {
+                    sb.append(CHARS[i]);
+                    sb.append(CHARS[i - 2]);
+                }
+            }
+            i += 2;
+        }
+        return sb.toString();
+    }
 }
